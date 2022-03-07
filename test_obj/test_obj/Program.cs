2163cf6e96b0f8f05793bc2800c67e2cf41b0813
jaynesh1701 <jaynesh1701@gmail.com>
2022-03-07 11:48:45 +0530

    additions docs

---
+++ test_obj/test_obj/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace test_obj
+{
+    class student
+    {
+        public int age;
+        public int id;
+        public string name;
+    }
+
+
+
+
+   class school
+    {
+
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            student s = new student();
+            s.age = 21;
+            s.id = 206;
+            s.name = "bhavya";
+            Console.WriteLine(s.age);
+            Console.WriteLine(s.name);
+            Console.WriteLine(s.id);
+            student a = new student();
+            a.age = 22;
+            a.id = 102;
+            a.name = "nitish";
+            Console.WriteLine(a.age);
+            Console.WriteLine(a.name);
+            Console.WriteLine(a.id);
+
+
+        }
+
+    }
+   
+}
